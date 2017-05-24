5eb2e4545a63e4a200c79fdce940b1adf69801b9
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-05-24 07:58:24 +0100

    remove wrapper around footer

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
 			<footer class="footer background--primary" role="contentinfo">
 				<a class="copyright"><?php bloginfo('name'); ?> <?php echo date('Y'); ?>.</a>
 			</footer>
-		</div>
-		<!-- /wrapper -->
 
 		<?php wp_footer(); ?>
 
