43cb7e11f7a9306ef98127d91877bbaf8599c254
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-05-24 07:58:06 +0100

    Creating a custom 404 page

--- 404.php
+++ 404.php
@@ -1,24 +1,11 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+	<main role="main" class="fourohfour">
+		<h1>
+			Oi! What are you doing over here?
+		</h1>
 
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
+		<h2><a href="<?php echo home_url(); ?>">Return Home</a></h2>
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
