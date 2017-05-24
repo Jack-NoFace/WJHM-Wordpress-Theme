ff72f7396a4071f652a98befd90e8b279213bac2
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-05-24 08:00:37 +0100

    Remove sidebar and add Grid container to all page with loops

--- category.php
+++ category.php
@@ -4,16 +4,18 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
+			<h1><?php _e( 'Posts from ', 'html5blank' ); single_cat_title(); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+			<section class="grid">
+
+				<?php get_template_part('loop'); ?>
+
+			</section>
 
 			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -6,14 +6,16 @@
 
 			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+			<section class="grid">
+
+				<?php get_template_part('loop'); ?>
+
+			</section>
 
 			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
