1e1aea6cacf7c88dae840c368c1f03a6e30269df
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-05-28 18:00:23 +0100

    Include Social Media Icons in Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 			<footer class="footer background--primary" role="contentinfo">
-				<a class="copyright"><?php bloginfo('name'); ?> <?php echo date('Y'); ?>.</a>
+				<ul class="list list--plain list--horizontal">
+					<li class="list__item margin-left--normal"><a href="https://www.behance.net/JLDavies" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#behance"></use></svg><span>Behance</span></a></li>
+					<li class="list__item margin-left--normal"><a href="https://codepen.io/JackDavies" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#codepen"></use></svg><span>Codepen</span></a></li>
+					<li class="list__item margin-left--normal"><a href="https://github.com/JackDaviesWork" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#github"></use></svg><span>Github</span></a></li>
+					<li class="list__item margin-left--normal"><a href="https://www.linkedin.com/in/jack-davies-5668b2108/" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#linkedin"></use></svg><span>Linkedin</span></a></li>
+					<li class="list__item margin-left--normal"><a href="mailto:jackdavieswork@hotmail.co.uk" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#microsoftoutlook"></use></svg><span>Outlook</span></a></li>
+					<li class="list__item margin-left--normal"><a href="https://twitter.com/whatjackhasmade" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#twitter"></use></svg><span>Twitter</span></a></li>
+					<li class="list__item margin-left--normal"><a href="https://www.youtube.com/channel/UCIOm-HME4V_STS9yWM5aXIg" class="icon"><svg class="icon" aria-hidden="true"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/sprite-sheet.svg#youtube"></use></svg><span>YouTube</span></a></li>
+				</ul>
 			</footer>
 
 		<?php wp_footer(); ?>
