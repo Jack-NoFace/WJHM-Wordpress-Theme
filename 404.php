469cb0114ab48bb23602e84d4c27e9d2daa6935c
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-05-28 18:00:14 +0100

    Update 404 Page

--- 404.php
+++ 404.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
 
-	<main role="main" class="fourohfour">
-		<h1>
-			Oi! What are you doing over here?
-		</h1>
+	<main role="main" class="padding--large">
+		<h1>Oi! What are you doing over here?</h1>
 
 		<h2><a href="<?php echo home_url(); ?>">Return Home</a></h2>
 	</main>
