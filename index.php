a568ee4c58d080902e3f9d9fa47a18bba2b4841b
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-01 22:07:29 +0100

    Include index to replicate heompage template

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<?php $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+$query = array(
+	'post_type' => 'post',
+	'posts_per_page' => 15,
+	'paged' => $paged
+);
 
-			<section class="grid">
+$pattern = 0;
 
-				<?php get_template_part('loop'); ?>
+$loop = new WP_Query( $query ); ?>
 
-			</section>
+<main role="main">
+	<!-- section -->
 
-			<?php get_template_part('pagination'); ?>
+<section>
 
-		</section>
-		<!-- /section -->
-	</main>
+<h1>WhatJackHasMade - Latest Posts</h1>
+
+<section class="grid">
+
+<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+	<?php get_template_part('partials/grid', 'item'); ?>
+
+<?php endwhile; ?>
+
+</section>
+
+</section>
+
+
+</main>
+
+<?php if ($loop->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
+	<nav class="pagination background--secondary">
+		<?php echo get_previous_posts_link( '<< Newer Entries' ); // display newer posts link ?>
+		<span><?php echo get_next_posts_link( 'Older Entries >>', $loop->max_num_pages ); // display older posts link ?></span>
+	</nav>
+<?php } ?>
+
+<?php wp_reset_query(); ?>
 
 <?php get_footer(); ?>
