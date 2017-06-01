eb47e6eb9969b428c5cc3e6ece081b7ee2b1cf85
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-06-01 15:31:46 +0100

    Organise grid item as a partial

--- loop.php
+++ loop.php
@@ -1,50 +1,6 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-	<!-- article -->
-	<article class="grid-item">
-		<?php
-
-		if ($pattern != 5) {
-			$pattern++;
-		} else {
-			$pattern = 1;
-		}
-		?>
-
-		<div id="post-<?php the_ID(); ?>" class="grid-item__thumbnail background--primary pattern--<?php echo $pattern;?>">
-		</div>
-
-		<div class="grid-item__details">
-			<h2><?php the_title(); ?></h2>
-
-			<?php $meta = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>
-			<?php
-
-			if ($meta) {
-				echo "<p>" . $meta . "</p>";
-			}
-
-			?>
-		</div>
-
-		<a class="link--cover" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">View Article</a>
-
-        <svg>
-            <style>
-			<?php if ( has_post_thumbnail()) { ?>
-	        <?php
-				$mobile = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'mobile' );
-			?>
-
-			@media screen and (min-width: 640px) {
-                #post-<?php the_ID(); ?> { background-image: url('<?php echo $mobile['0']; ?>'); }
-			}
-            </style>
-        </svg>
-        <?php }?>
-
-	</article>
-	<!-- /article -->
+	<?php get_template_part('partials/grid', 'item'); ?>
 
 <?php endwhile; ?>
 
--- page-homepage.php
+++ page-homepage.php
@@ -23,51 +23,9 @@ $loop = new WP_Query( $query ); ?>
 
 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-	<article class="grid-item">
-		<?php
+	<?php get_template_part('partials/grid', 'item'); ?>
 
-		if ($pattern != 5) {
-			$pattern++;
-		} else {
-			$pattern = 1;
-		}
-		?>
-
-		<div id="post-<?php the_ID(); ?>" class="grid-item__thumbnail background--primary pattern--<?php echo $pattern;?>">
-		</div>
-
-		<div class="grid-item__details">
-			<h2><?php the_title(); ?></h2>
-
-			<?php $meta = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>
-			<?php
-
-			if ($meta) {
-				echo "<p>" . $meta . "</p>";
-			}
-
-			?>
-		</div>
-
-		<a class="link--cover" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">View Article</a>
-
-        <svg>
-            <style>
-			<?php if ( has_post_thumbnail()) { ?>
-	        <?php
-				$mobile = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'mobile' );
-			?>
-
-			@media screen and (min-width: 640px) {
-                #post-<?php the_ID(); ?> { background-image: url('<?php echo $mobile['0']; ?>'); }
-			}
-            </style>
-        </svg>
-        <?php }?>
-
-	</article>
-
-	<?php endwhile; ?>
+<?php endwhile; ?>
 
 </section>
 
