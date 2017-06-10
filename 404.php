676583850b800803160559636e62b952041e3668
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-10 20:55:24 +0100

    Fix heading colour on 404

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 	<main role="main" class="padding--large">
-		<h1>Oi! What are you doing over here?</h1>
+		<h1 class="color--white">Oi! What are you doing over here?</h1>
 
-		<h2><a href="<?php echo home_url(); ?>">Return Home</a></h2>
+		<h2><a href="<?php echo home_url(); ?>" class="link--fancy">Return Home</a></h2>
 	</main>
 
 <?php get_footer(); ?>
