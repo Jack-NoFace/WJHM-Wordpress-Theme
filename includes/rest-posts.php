e480646840a48a9bebebb0fba237108df6f89adc
Jack Pritchard <jack@noface.co.uk>
2019-03-24 17:54:51 +0000

    ISO Dates please

--- includes/rest-posts.php
+++ includes/rest-posts.php
@@ -45,7 +45,7 @@ function rest_posts($data)
             array_push(
                 $insightItems, array(
                     'content' => $the_content,
-                    'date' => get_the_date('c'),
+                    'date' => the_time('c'),
                     'excerpt' => get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true),
                     'id' => get_the_ID(),
                     'imageLargest' => get_the_post_thumbnail_url(get_the_ID(), 'largest'),
