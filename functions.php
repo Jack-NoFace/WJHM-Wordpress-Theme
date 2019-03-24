86c7757983923bc75acaacc5359506bcd55c7d4b
Jack Pritchard <jack@noface.co.uk>
2019-03-24 16:59:10 +0000

    Spelling mistake, DOH

--- functions.php
+++ functions.php
@@ -384,7 +384,7 @@ class StarterSite extends Timber\Site
         include_once 'includes/functions/get-acf-titles.php';
         include_once 'includes/functions/convert-the-content.php';
 
-        include_once 'includes/rest-event.php';
+        include_once 'includes/rest-events.php';
         include_once 'includes/rest-inspiration.php';
         include_once 'includes/rest-journal.php';
         include_once 'includes/rest-menus.php';
