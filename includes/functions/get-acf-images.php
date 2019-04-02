ed8b3b76c4c5ad26245cc08f87111da72d799056
Jack Pritchard <jack@noface.co.uk>
2019-04-02 17:11:04 +0100

    Load video media URLs

--- includes/functions/get-acf-images.php
+++ includes/functions/get-acf-images.php
@@ -1,10 +1,15 @@
 <?php
+// // Setup postdata allowing get_field() to work.
+// acf_setup_postdata($block['data'], $block['id'], true);
+// // get_fields()
+// acf_reset_postdata($block['id']);
 
 function getACFImages($content)
 {
     if (!empty($content)):
 
         $imageIDs = [];
+        $videoIDs = [];
 
         $array_obj = new RecursiveIteratorIterator(new RecursiveArrayIterator($content));
         foreach ($array_obj as $key => $value) {
@@ -12,8 +17,13 @@ function getACFImages($content)
                 if (wp_get_attachment_image_src($value, 'full')):
                     $imageIDs[] = $value;
                 endif;
+                if (!wp_get_attachment_image_src($value, 'full') && wp_get_attachment_url($value)):
+                    $videoIDs[] = $value;
+                endif;
             endif;
         }
+
+        $videoIDs = array_unique($videoIDs);
 
         /* Set up an empty array for the links. */
         $links = array();
@@ -23,7 +33,6 @@ function getACFImages($content)
         $sizes[] = 'full';
 
         foreach ($imageIDs as $media) {
-
             /* Loop through each of the image sizes. */
             foreach ($sizes as $size) {
 
@@ -38,11 +47,16 @@ function getACFImages($content)
             }
         }
 
-        array_walk_recursive($content, function (&$value, $key) use ($links) {
+        array_walk_recursive($content, function (&$value, $key) use ($links, $videoIDs) {
             if (!is_array($value) && $key === 'media') {
                 foreach ($links as $imageKey => $imageValue) {
                     if ($value === $imageKey) {
                         $value = $imageValue;
+                    }
+                }
+                foreach ($videoIDs as $videoKey => $videoValue) {
+                    if ($value === $videoValue) {
+                        $value = wp_get_attachment_url($videoValue);
                     }
                 }
             }
