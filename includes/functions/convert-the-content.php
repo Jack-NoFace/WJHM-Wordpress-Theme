a110961bb9f39bd8806726c71b995aae6a392c53
Jack Pritchard <jack@noface.co.uk>
2019-04-04 09:11:24 +0100

    Fix attribute bug

--- includes/functions/convert-the-content.php
+++ includes/functions/convert-the-content.php
@@ -15,24 +15,26 @@ function convert_content($content)
     $content = getACFImages($content);
 
     foreach ($content as &$block) {
-        // Setup postdata allowing get_field() to work.
-        acf_setup_postdata($block['attrs']['data'], $block['attrs']['id'], true);
+        if ($block['attrs']) {
+            // Setup postdata allowing get_field() to work.
+            acf_setup_postdata($block['attrs']['data'], $block['attrs']['id'], true);
 
-        // get_fields()
-        if ($block['blockName'] === "acf/testimonials") {
-            $testimonials = $block['attrs']['data']['testimonials'];
+            // get_fields()
+            if ($block['blockName'] === "acf/testimonials") {
+                $testimonials = $block['attrs']['data']['testimonials'];
 
-            foreach ($testimonials as $value) {
-                $valueObject = json_encode($value, true);
-                $valueObject = json_decode($valueObject, true);
-                $testimonialObjects[] = $valueObject;
+                foreach ($testimonials as $value) {
+                    $valueObject = json_encode($value, true);
+                    $valueObject = json_decode($valueObject, true);
+                    $testimonialObjects[] = $valueObject;
+                }
+
+                $block['attrs']['data']['testimonials'] = $testimonialObjects;
             }
 
-            $block['attrs']['data']['testimonials'] = $testimonialObjects;
+            // reset_loop()
+            acf_reset_postdata($block['attrs']['id']);
         }
-
-        // reset_loop()
-        acf_reset_postdata($block['attrs']['id']);
     }
 
     unset($block);
