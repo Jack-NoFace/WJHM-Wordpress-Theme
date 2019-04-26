9676ba421df36f29714652d429a7c38493d3c84b
Jack Pritchard <jack@noface.co.uk>
2019-04-26 22:44:44 +0100

    Start case study pages

--- functions.php
+++ functions.php
@@ -62,6 +62,41 @@ class StarterSite extends Timber\Site
     /** This is where you can register custom post types. */
     public function register_post_types()
     {
+        register_taxonomy_for_object_type('category', 'case');
+        register_taxonomy_for_object_type('post_tag', 'case');
+        register_post_type('case',
+            array(
+                'labels' => array(
+                    'name' => __('Case Study', 'case'),
+                    'singular_name' => __('Case Study', 'case'),
+                    'add_new' => __('Add New', 'case'),
+                    'add_new_item' => __('Add New Case Study', 'case'),
+                    'edit' => __('Edit', 'case'),
+                    'edit_item' => __('Edit Case Study', 'case'),
+                    'new_item' => __('New Case Study', 'case'),
+                    'view' => __('View Case Study', 'case'),
+                    'view_item' => __('View Case Study', 'case'),
+                    'search_items' => __('Search Case Study', 'case'),
+                    'not_found' => __('No Case Studies found', 'case'),
+                    'not_found_in_trash' => __('No Case Studies found in Trash', 'case'),
+                ),
+                'public' => true,
+                'hierarchical' => true,
+                'has_archive' => true,
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'thumbnail',
+                ),
+                'show_in_rest' => true,
+                'menu_icon' => 'dashicons-media-document',
+                'can_export' => true,
+                'taxonomies' => array(
+                    'post_tag',
+                    'category',
+                ),
+            ));
+
         register_taxonomy_for_object_type('category', 'event');
         register_taxonomy_for_object_type('post_tag', 'event');
         register_post_type('event',
@@ -269,6 +304,7 @@ class StarterSite extends Timber\Site
         include_once 'includes/functions/get-acf-titles.php';
         include_once 'includes/functions/convert-the-content.php';
 
+        include_once 'includes/rest-cases.php';
         include_once 'includes/rest-events.php';
         include_once 'includes/rest-inspiration.php';
         include_once 'includes/rest-menus.php';
